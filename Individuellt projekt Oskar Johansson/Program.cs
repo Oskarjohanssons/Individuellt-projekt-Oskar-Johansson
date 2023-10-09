8b31a4500d22eb7ae715cafe8621690ec8a770f8
oskar <oskar@OSKAR>
2023-10-09 13:04:06 +0200

    Add project files.

---
+++ Individuellt projekt Oskar Johansson/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Individuellt_projekt_Oskar_Johansson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
