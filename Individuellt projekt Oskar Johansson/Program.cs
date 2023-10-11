72a9b3ee203ee07e8d28862e09051680f3181d71
Oskar Johansson <oskar.tzar@hotmail.com>
2023-10-11 15:48:02 +0200

    Har gjort en egen klass som heter ATM där alla metoder ska ligga i och som jag sedan kallar på i main metoden. Har även börjat på några metoder som Welcome, LogIn och en privat metod som heter SucccessfulLogIn som jag även kallar på i min LogIn metod.

--- Individuellt projekt Oskar Johansson/Program.cs
+++ Individuellt projekt Oskar Johansson/Program.cs
@@ -1,23 +1,72 @@
 ﻿namespace Individuellt_projekt_Oskar_Johansson
 {
-    internal class Program
+    class ATM
     {
-        public void ATM()
-        {
+        private string[] UserName = { "Oskar", "Emilia", "Vilgot", "Fia", "Felix" };
+        private string[] Pins = { "2233", "1122", "1234", "0000", "1111" };
+        private string[] AccountNames = { "Primärtkonto", "Sparkonto" };
+        private double[][] AccountsBalances = new double[5][];
 
+        public ATM()
+        {
+            for (int i = 0; i < UserName.Length; i++)
+            {
+                AccountsBalances[i] = new double[2];
+                for (int j = 0; j < 2; j++)
+                {
+                    AccountsBalances[i][j] = 100.0;
+                }
+            }
         }
 
         public void Welcome()
         {
-
+            Console.WriteLine("Välkommen till din bank");
         }
 
-        public void LogIn()
+        public string LogIn()
         {
+            int LogInAttempts = 3;
+            string username, pin;
+
+            do
+            {
+                Console.WriteLine("Ange ditt användarnamn: ");
+                username = Console.ReadLine();
+                Console.WriteLine("Ange din pinkod: ");
+                pin = Console.ReadLine();
+
+                if (SuccessfulLogIn(username, pin))
+                {
+                    return username;
+                }
+                else
+                {
+                    LogInAttempts--;
+                    Console.WriteLine($"Fel användarnamn eller pin. Försök igen. du har {LogInAttempts} kvar");
+                }
+            }
+            
+            while (LogInAttempts >= 0);
+
+            Console.WriteLine("Du har skrivit in fel pinkod tre gånger. Programmet kommer att avslutas nu.");
+            Environment.Exit(0);
+            return null;
 
         }
+        private bool SuccessfulLogIn(string username, string pin)
+        {
+            for(int i = 0;i < UserName.Length;i++)
+            {
+                if (UserName[i] == username && Pins[i] == pin)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
 
-        public void MainMenu()
+        public void MainMenu(string username)
         {
 
         }
@@ -35,10 +84,16 @@
         {
 
         }
+    }
+    internal class Program
+    {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("");
+            ATM Bank = new ATM();
+            Bank.Welcome();
+            string username = Bank.LogIn();
+            Bank.MainMenu(username);
         }
+
     }
 }
